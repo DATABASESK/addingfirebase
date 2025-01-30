495e500559231b061e42d7d9e927259ebe486032
DATABASESK <server1.2148652652@gmail.com>
2025-01-30 21:11:27 +0530

    Update not-found.js

--- app/not-found.js
+++ app/not-found.js
@@ -1,8 +1,12 @@
 export default function Custom404() {
   return (
     <>
-      <div className="w-full h-screen bg-white flex items-center justify-center">
-        <p className="text-4xl text-black">Hello!</p>
+      <div className="w-full h-screen bg-black flex items-center justify-center text-center px-4">
+        <div className="text-lg text-white">
+          <p>This website is a movie platform created as part of a college project.</p>
+          <p>It is built solely for educational purposes and is not intended for commercial use or sale.</p>
+          <p>Please enjoy browsing the content!</p>
+        </div>
       </div>
     </>
   );
