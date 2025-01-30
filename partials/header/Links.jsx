6511b1b3a015fe9e9eb8f47868c258857cae205b
DATABASESK <server1.2148652652@gmail.com>
2025-01-30 21:37:01 +0530

    Update Links.jsx

--- partials/header/Links.jsx
+++ partials/header/Links.jsx
@@ -6,27 +6,21 @@ import styles from "./header.module.css"
 const Links = ({ isMobile }) => {
   const pathname = usePathname()
 
-
-
   const links = [
     "Home",
     "Catalog",
     "About"
-
-    
-   
   ]
 
   if (isMobile) {
     return (
-      <div className="flex flex-col h-full justify-between items-center  text-[#c4c2c7] p-2 gap-1 overflow-hidden ">
+      <div className="flex flex-col h-full justify-between items-center  text-[#c4c2c7] p-2 gap-2 overflow-hidden">
         {links.map((link, index) => (
           <Link
             href={link === "Home" ? "/" : link.toLowerCase()}
             key={link}
             className={`${(pathname === "/" ? "Home" : pathname).includes(link) ? "text-white bg-[#242233] border-2 border-[#313e5038]" : ""}  w-full h-full text-center py-[6px] rounded-md hover:bg-[#242233] border-2 border-transparent hover:border-[#313e5038] relative ${styles.animate_ltr}`}
             style={{ animationDelay: `${index * 0.13}s` }}
-
           >
             {link}
           </Link>
@@ -41,7 +35,7 @@ const Links = ({ isMobile }) => {
         <Link
           href={link === "Home" ? "/" : link === "Trending" ? "/catalog?sort=TRENDING_DESC" : `/${link.toLowerCase()}`}
           key={link}
-          className={`${index === 0 ? "ml-6" : "ml-4"} ${(pathname === "/" ? "home" : pathname).includes(link.toLowerCase()) ? "text-white" : ""}`}
+          className={`${index === 0 ? "ml-6" : "ml-6"} ${(pathname === "/" ? "home" : pathname).includes(link.toLowerCase()) ? "text-white" : ""}`}
         >
           {link}
         </Link>
