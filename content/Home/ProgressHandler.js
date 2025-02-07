afba425ede6bb31ee8492d6115cf71e452860522
DATABASESK <server1.2148652652@gmail.com>
2025-02-07 22:50:27 +0530

    Update ProgressHandler.js

--- content/Home/ProgressHandler.js
+++ content/Home/ProgressHandler.js
@@ -1,5 +1,7 @@
 export const removeWatchProgress = (id) => {
   const data = JSON.parse(localStorage.getItem("watchProgress")) || [];
   const updatedData = data.filter(item => item.id !== id);
+  
+  // Update localStorage
   localStorage.setItem("watchProgress", JSON.stringify(updatedData));
 };
