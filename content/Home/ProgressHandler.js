9f97b1fab042cdf401bcbaa6c763977ab9349e73
DATABASESK <server1.2148652652@gmail.com>
2025-02-07 22:57:35 +0530

    Update ProgressHandler.js

--- content/Home/ProgressHandler.js
+++ content/Home/ProgressHandler.js
@@ -1,11 +1,19 @@
+// Ensure these functions run on the client
 export const getWatchProgress = () => {
-  return JSON.parse(localStorage.getItem("watchProgress")) || [];
+  if (typeof window !== "undefined") {
+    // Parse localStorage data or return an empty array if none exists
+    return JSON.parse(localStorage.getItem("watchProgress")) || [];
+  }
+  return [];
 };
 
 export const removeWatchProgress = (id) => {
-  let data = getWatchProgress();
-  let updatedData = data.filter(item => item.id !== id);
-  
-  // Update localStorage to permanently remove movie
-  localStorage.setItem("watchProgress", JSON.stringify(updatedData));
+  if (typeof window !== "undefined") {
+    // Get the current watch progress
+    const data = JSON.parse(localStorage.getItem("watchProgress")) || [];
+    // Filter out the item with the matching id
+    const updatedData = data.filter(item => item.id !== id);
+    // Update localStorage with the new data array
+    localStorage.setItem("watchProgress", JSON.stringify(updatedData));
+  }
 };
