62a832db4af89b3cc7bf8487512edec98b4df582
DATABASESK <server1.2148652652@gmail.com>
2025-02-08 11:03:02 +0530

    Update ContinueWatchingCard.jsx

--- components/Cards/ContinueWatchingCard/ContinueWatchingCard.jsx
+++ components/Cards/ContinueWatchingCard/ContinueWatchingCard.jsx
@@ -1,14 +1,18 @@
-"use client"
-import Image from "next/image"
+"use client";
+import Image from "next/image";
 import Link from "next/link";
-import { FaPlay } from "react-icons/fa6";
+import { FaPlay, FaTrashAlt } from "react-icons/fa";
+import { removeFromWatchHistory } from "@/utils/ProgressHandler";
 
 const ContinueWatchingCard = ({ data, hidden }) => {
-
   if (hidden) {
-    return <div className=""></div>
+    return <div className=""></div>;
   }
 
+  const handleRemove = () => {
+    removeFromWatchHistory(data?.id);
+    window.location.reload(); // Reload the page to reflect changes
+  };
 
   return (
     <Link
@@ -25,32 +29,38 @@ const ContinueWatchingCard = ({ data, hidden }) => {
 
       <div className="w-full h-20 absolute flex flex-col justify-between bottom-0 after:content-[''] after:w-full after:h-28 after:absolute after:bottom-0 after:bg-[linear-gradient(360deg,#060602,#00000069,transparent)]">
         <div className="flex flex-col mx-3 justify-between gap-3 z-10">
-
-          <div className="flex items-center justify-between w-full ">
+          <div className="flex items-center justify-between w-full">
             <div>
-              <div className="text-white w-full text-wrap break-words overflow-hidden text-ellipsis line-clamp-1 font-['poppins'] text-lg cursor-pointer hover:text-slate-200">{data?.title}</div>
-              <div className="text-[#ffffff8a] font-['poppins'] text-[14px]">Episode: {data?.episode}</div>
+              <div className="text-white w-full text-wrap break-words overflow-hidden text-ellipsis line-clamp-1 font-['poppins'] text-lg cursor-pointer hover:text-slate-200">
+                {data?.title}
+              </div>
+              <div className="text-[#ffffff8a] font-['poppins'] text-[14px]">
+                Episode: {data?.episode}
+              </div>
             </div>
 
-            <div className="p-[13px] rounded-full flex items-center justify-center bg-[#1a212bd0] text-[#8c97a7] cursor-pointer border border-[#242b35] backdrop-blur-sm hover:bg-[#2c3440d0] hover:text-[#abbcd5] duration-100 ">
+            <div className="p-[13px] rounded-full flex items-center justify-center bg-[#1a212bd0] text-[#8c97a7] cursor-pointer border border-[#242b35] backdrop-blur-sm hover:bg-[#2c3440d0] hover:text-[#abbcd5] duration-100">
               <FaPlay />
             </div>
-
           </div>
 
           <div className="w-full bg-[#404141] h-1 rounded-md">
             <div
               className="h-full bg-[#dd8dae] rounded-md"
-              style={{ width: `${(data?.episode * 100 / data?.totalepisode) || 0}%` }}
+              style={{ width: `${(data?.episode * 100) / data?.totalepisode || 0}%` }}
             ></div>
           </div>
-
         </div>
+      </div>
 
-
+      <div
+        className="absolute top-2 right-2 p-2 cursor-pointer text-white"
+        onClick={handleRemove}
+      >
+        <FaTrashAlt />
       </div>
     </Link>
-  )
-}
+  );
+};
 
-export default ContinueWatchingCard
+export default ContinueWatchingCard;
