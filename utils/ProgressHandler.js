f93a0c1f0c2697fc0d1c2f57129d58a9399602ab
DATABASESK <server1.2148652652@gmail.com>
2025-02-08 11:02:07 +0530

    Update ProgressHandler.js

--- utils/ProgressHandler.js
+++ utils/ProgressHandler.js
@@ -35,38 +35,36 @@ export const getWatchProgress = (isSlice = true, page = 1, pageSize = 4) => {
   return data;
 };
 
-export const saveWatchProgress = (
-  data,
-  episodes,
-  episode,
-
-) => {
-  const existed_data = JSON.parse(localStorage.getItem("watch_history") || "[]")
-  const id = data?.id
-
+export const saveWatchProgress = (data, episodes, episode) => {
+  const existed_data = JSON.parse(localStorage.getItem("watch_history") || "[]");
+  const id = data?.id;
 
   if (episodes.length === 0 || episodes[episode - 1]?.title === 0) {
-    return
+    return;
   }
 
-
-  const episodeInfo = episodes[episode - 1]
-  // console.log(data, episodeInfo)
+  const episodeInfo = episodes[episode - 1];
 
   const historyData = {
     [id]: {
-      title: ((episodeInfo?.title || "") || (data?.original_name) || (data?.title)),
+      title: episodeInfo?.title || data?.original_name || data?.title,
       episode_number: episodeInfo?.episode_number || 1,
       season_number: episodeInfo?.season_number || 1,
-      totalepisode: (episodes?.length) || 1,
+      totalepisode: episodes?.length || 1,
       description: episodeInfo?.overview || "",
       runtime: episodeInfo?.runtime || 1,
       media_type: data?.type || "movie",
       date: Date.now(),
       movieid: id,
       thumbnail: `https://image.tmdb.org/t/p/w250_and_h141_bestv2${episodeInfo?.still_path || ""}`,
-    }
-  }
+    },
+  };
 
-  localStorage.setItem("watch_history", JSON.stringify({ ...existed_data, ...historyData }))
-}
+  localStorage.setItem("watch_history", JSON.stringify({ ...existed_data, ...historyData }));
+};
+
+export const removeFromWatchHistory = (id) => {
+  const movieData = JSON.parse(localStorage.getItem("watch_history") || "{}");
+  delete movieData[id];
+  localStorage.setItem("watch_history", JSON.stringify(movieData));
+};
