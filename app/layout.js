c7afd10aa4180e79ac82335143b3bc7c4dfde761
DATABASESK <server1.2148652652@gmail.com>
2025-02-08 11:08:38 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,29 +6,19 @@ import "./globals.css";
 import Header from "@/partials/header/Header";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import LoginModal from "@/components/LoginModal";  // Import Login Modal
 import AnalyticsWrapper from "@/components/AnalyticsWrapper";  // Import new wrapper
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
-  const [isLoggedIn, setIsLoggedIn] = useState(false); // Manage login state
-
-  const handleLogin = () => {
-    setIsLoggedIn(true); // Update state to indicate the user is logged in
-  };
-
   return (
     <html lang="en">
       <body className={inter.className}>
         <Header />
-
-        {/* Conditionally render the login modal and apply blur */}
-        {!isLoggedIn && <LoginModal onLogin={handleLogin} />}  {/* Show login modal if not logged in */}
         
-        {/* Conditionally blur the content */}
-        <div className={`transition-all duration-300 ${!isLoggedIn ? "blur-sm" : ""}`}>
-          {children} {/* Main content of the page */}
+        {/* Main content of the page */}
+        <div>
+          {children}
         </div>
 
         <ToastContainer draggable theme="dark" />
