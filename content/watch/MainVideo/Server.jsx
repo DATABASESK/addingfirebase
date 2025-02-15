efd53726090fde073f6bae4e3b5ac36d08caae19
DATABASESK <server1.2148652652@gmail.com>
2025-02-15 12:25:29 +0530

    Update Server.jsx

--- content/watch/MainVideo/Server.jsx
+++ content/watch/MainVideo/Server.jsx
@@ -23,7 +23,7 @@ const Server = () => {
   const TVVideoPlayers = {
 
     vidsrcpro: `https://vidsrc.pro/embed/tv/${MovieId}/${season}/${episode}`,
-    streamurl:  `https://moviereqapi.onrender.com/play/s{imdb_id}?d=pikachu.app&?sinku&noseasons=${Season}&noepisodes=${episode}
+    streamurl:  `https://moviereqapi.onrender.com/play/s{imdb_id}?d=pikachu.app&?sinku&noseasons=${Season}&noepisodes=${episode}`,
 
     "vidsrc.in": `https://vidsrc.in/embed/tv/${MovieId}/${season}/${episode}`,
     "vidsrc.me": `https://vidsrc.me/v2/embed/tv/${MovieId}/${season}/${episode}`,
