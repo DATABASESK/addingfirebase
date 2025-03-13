8963398e9164a5414b89b7b4f346dbf6b8cd385c
DATABASESK <server1.2148652652@gmail.com>
2025-03-13 21:30:32 +0530

    Update VideoPlayerContainer.jsx

--- content/watch/MainVideo/videoPlayer/VideoPlayerContainer.jsx
+++ content/watch/MainVideo/videoPlayer/VideoPlayerContainer.jsx
@@ -3,29 +3,18 @@ import LoadingVideo from '@/components/loadings/loadingVideo/loadingVideo';
 import { useWatchContext } from '@/context/Watch';
 import { useWatchSettingContext } from '@/context/WatchSetting';
 import MainVideoPlayer from './MainVideoPlayer';
-import { useEffect, useState } from 'react';
 
 const VideoPlayerContainer = () => {
-  const { watchInfo, MovieInfo, episode, season } = useWatchContext();
+  const { watchInfo, MovieInfo } = useWatchContext();
   const { watchSetting, setWatchSetting } = useWatchSettingContext();
-  const [currentVideoInfo, setCurrentVideoInfo] = useState(watchInfo);
-
-  // Update the video when episode or season changes
-  useEffect(() => {
-    setCurrentVideoInfo(watchInfo);
-  }, [watchInfo, episode, season]);
 
   return (
     <>
-      <div className="z-30">
+      <div className='z-30'>
         {watchInfo?.loading ? (
           <LoadingVideo />
         ) : (
-          <MainVideoPlayer
-            key={`${MovieInfo?.id}-${season}-${episode}`} // Forces re-render
-            videoInfo={currentVideoInfo}
-            movieInfo={MovieInfo}
-          />
+          <MainVideoPlayer videoInfo={watchInfo} movieInfo={MovieInfo} />
         )}
       </div>
 
@@ -35,9 +24,9 @@ const VideoPlayerContainer = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="fixed top-0 left-0 w-full h-full z-20 bg-[#000000e5]"
-            onClick={() => setWatchSetting((prev) => ({ ...prev, light: false }))}
-          />
+            className='fixed top-0 left-0 w-full h-full z-20 bg-[#000000e5]'
+            onClick={() => setWatchSetting(prev => ({ ...prev, light: false }))}
+          ></motion.div>
         ) : null}
       </AnimatePresence>
     </>
