e8fecd436d72610ff24bac2f0c84cd6dc4d2120a
DATABASESK <server1.2148652652@gmail.com>
2025-03-13 22:14:30 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,24 +7,36 @@ import WatchHistory from "@/content/Home/WatchHistory";
 import Tamil from "@/content/Home/Tamil";
 import Horror from "@/content/Home/Horror";
 import Romantic from "@/content/Home/Romantic";
-import RecentTamil from "@/content/Home/RecentTamil"; // Import Recent Tamil Movies
+import RecentTamil from "@/content/Home/RecentTamil"; 
+import TopNetflixSeries from "@/content/Home/TopNetflixSeries"; // ✅ Import Top Netflix Series
+
 import { 
   getTrendingMovies, 
   getTopRatedMovies, 
   getTamilTrendingMovies, 
   getHorrorMovies, 
   getRomanticMovies, 
-  getRecentTamilMovies 
+  getRecentTamilMovies,
+  getTopNetflixSeriesGlobal // ✅ Fetch Function for Netflix Series
 } from "@/lib/MoviesFunctions";
 
 const Home = async () => {
-  const [trendingdata, top_rateddata, tamilMovies, horrorMovies, romanticMovies, recentTamilMovies] = await Promise.all([
+  const [
+    trendingdata, 
+    top_rateddata, 
+    tamilMovies, 
+    horrorMovies, 
+    romanticMovies, 
+    recentTamilMovies,
+    topNetflixSeries // ✅ Fetching Netflix Series
+  ] = await Promise.all([
     getTrendingMovies(),
     getTopRatedMovies(),
     getTamilTrendingMovies(),
     getHorrorMovies(),
     getRomanticMovies(),
-    getRecentTamilMovies(), // Fetch recent Tamil movies
+    getRecentTamilMovies(),
+    getTopNetflixSeriesGlobal() // ✅ Fetching Top Netflix Series
   ]);
 
   return (
@@ -34,7 +46,8 @@ const Home = async () => {
       {/* Global Spacing Added */}
       <div className="w-full flex flex-col items-center z-10 relative main-responsive space-y-16 mt-16">
         <Trending data={trendingdata} />
-        <RecentTamil data={recentTamilMovies} /> {/* Recent Tamil Movies (Moved to 2nd Place) */}
+        <TopNetflixSeries data={topNetflixSeries} /> {/* ✅ Added Netflix Series */}
+        <RecentTamil data={recentTamilMovies} />
         <WatchHistory />
         <Collection />
         <Popular />
@@ -44,7 +57,7 @@ const Home = async () => {
         <Romantic data={romanticMovies} />
       </div>
 
-      {/* background */}
+      {/* Background Effects */}
       <div className="fixed w-[138.33px] h-[82.25px] left-[1%] top-[2%] bg-[#92b7fc8f] blur-[200px]"></div>
       <div className="fixed w-[500px] h-[370.13px] right-[50%] bottom-[20%] bg-[#576683b4] blur-[215.03px] translate-x-[70%] z-0 rounded-full"></div>
     </>
