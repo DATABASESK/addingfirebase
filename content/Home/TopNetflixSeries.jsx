fde21ac70a878a1781bf72962e91d23b3b02ab0a
DATABASESK <server1.2148652652@gmail.com>
2025-03-14 23:00:24 +0530

    Update TopNetflixSeries.jsx

--- content/Home/TopNetflixSeries.jsx
+++ content/Home/TopNetflixSeries.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Card from "@/components/Cards/Card/Card";
-import { getTopNetflixSeriesGlobal } from "@/lib/MoviesFunctions";
+import { getTopNetflixSeriesGlobal } from "@/lib/TvFunctions";
 import { useEffect, useState } from "react";
 
 const TopNetflixSeries = () => {
